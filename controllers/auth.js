52a7aad44c08c5a8d64a8d5bf7720a195de104da
Rakib Khan <rakibkhan@live.co.uk>
2020-03-30 17:07:06 +0100

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -52,3 +52,25 @@ exports.requireSignin = expressJwt({
     secret: 'sjsiojsio',
     userProperty: 'auth'
 })
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id
+
+    if (!user) {
+        return res.status(403).json({
+            error: 'access denied'
+        });
+    }
+
+    next();
+}
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: 'admin only'
+        });
+    }
+
+    next();
+}
